5ac9854614d0616f7f3d517e6e91c6c334358717
hadryx <hadrien.belkebir@gmail.com>
2013-10-15 12:16:59 +0200

    Modification du Main pour test

--- src/main.c
+++ src/main.c
@@ -17,15 +17,20 @@ int main() {
     TypVoisin * l = NULL;
     TypGraphe g;
     g = initGraph(5);
-    
-    ajouterElement(&l,2,6);
-    ajouterElement(&l,6,2);
-    ajouterElement(&l,9,9);
-    ajouterElement(&l,3,2);
 
-    afficherListe(l);
-    /*SupprimerElement(&l,3);
-    AfficherListe(l);*/
+    
+    insertionSommet(&g,1);
+    insertionSommet(&g,2);
+    insertionSommet(&g,3);
+    insertionSommet(&g,4);
+    insertionArete(&g,1,2,5,0);
+    insertionArete(&g,1,3,2,1);
+    insertionArete(&g,9,1,2,0);
+    insertionArete(&g,1,0,9,0);
+    /*insertionSommet(&g,8);
+    insertionArete(&g,5,3,2,0);
+    */
+
    
     while (quitter != 1) {
         if (codeRetour) {
